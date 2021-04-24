9d40b1fde4f74e0694955bdf756965c37f410698
zuikaru <pavat.tangmo@hotmail.com>
2021-04-24 16:10:08 +0700

    Fix bug with role guard

--- backend/src/auth/role/roles.decorator.ts
+++ backend/src/auth/role/roles.decorator.ts
@@ -1,5 +1,7 @@
-import { SetMetadata } from '@nestjs/common';
+import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 import { Role } from './role.enum';
+import { RolesGuard } from './roles.guard';
 
 export const ROLES_KEY = 'roles';
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: Role[]) =>
+    applyDecorators(SetMetadata(ROLES_KEY, roles), UseGuards(RolesGuard));
